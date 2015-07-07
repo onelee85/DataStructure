f3eb33a8621040b93a2dc23f8d8cf0f632efdc81
jiao.li <jiao.li@2339.com>
2015-07-07 18:49:04 +0800

    graph

--- src/main/java/com/james/ds/graph/Graph.java
+++ src/main/java/com/james/ds/graph/Graph.java
@@ -1,5 +1,11 @@
 package com.james.ds.graph;
 
+import com.james.ds.list.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static com.james.ds.Utils.pln;
 import static com.james.ds.Utils.pn;
 
@@ -14,7 +20,7 @@ import static com.james.ds.Utils.pn;
  */
 public class Graph {
 
-
+    private Integer nums = 0;
     //邻接矩阵：G[N][N]二位矩阵，适合稠密图，稀疏图则存在大量无效元素,统计边的条数比较浪费时间
     private Integer[][] graphs = null;
 
@@ -30,6 +36,7 @@ public class Graph {
 
     }
     public Graph(int n){
+        this.nums = n;
         this.graphs = new Integer[n][n];
         for (int i = 0; i < graphs.length; i++){
             for (int j = 0; j < graphs[i].length; j++){
@@ -40,6 +47,14 @@ public class Graph {
 
     }
 
+    /**
+     * 节点数量
+     * @return
+     */
+    private Integer getNum(){
+        return this.nums;
+    }
+
     public void insert(int v1, int v2){
         graphs[v1][v2] = 1;
         graphs[v2][v1] = 1;
@@ -47,7 +62,7 @@ public class Graph {
 
     public void insertGraphLink(int v1, int v2){
         Node node1 = graphLink[v1];
-        Node node2 = new Node(v2);
+        Node node1_2 = new Node(v2);
         if(node1 == null){
             node1 = new Node(v1);
             graphLink[v1] = node1;
@@ -55,7 +70,32 @@ public class Graph {
         while(node1.next != null){
             node1 = node1.next;
         }
-        node1.next = node2;
+        node1.next = node1_2;
+
+        Node node2 = graphLink[v2];
+        Node node2_1 = new Node(v1);
+        if(node2 == null){
+            node2 = new Node(v2);
+            graphLink[v2] = node2;
+        }
+        while(node2.next != null){
+            node2 = node2.next;
+        }
+        node2.next = node2_1;
+    }
+
+    /**
+     * 寻找节点 从邻接表
+     * @param n
+     * @return
+     */
+    public Node findGraphLink(int n){
+        for (int i = 0; i < graphLink.length; i++){
+            Node node = graphLink[i];
+            if(node != null && node.data == n)
+                return node;
+        }
+        return null;
     }
 
     public void print(){
@@ -74,7 +114,51 @@ public class Graph {
                 pn(node.data + " ");
                 node = node.next;
             }
-            pln();
+            if(graphLink[i] != null)
+                pln();
         }
     }
+
+    /**
+     * 深度优先
+     * 邻接表 O(N + E)
+     * 邻接矩阵 O(N^2)
+     */
+    public void DFS(int start_num) {
+        //找到起点
+        List<Integer> visited = new ArrayList<Integer>(nums);
+        Node v_node = findGraphLink(start_num);
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(v_node);
+        while (v_node != null || !stack.isEmpty()) {
+            if (v_node != null) {
+                v_node = findGraphLink(v_node.data);
+                visit(v_node);
+                visited.add(v_node.data);
+                Node next_node = v_node.next;
+                while (next_node != null && visited.contains(next_node.data)) {
+                    next_node = next_node.next;
+                }
+                v_node = next_node;
+            }
+            if (v_node != null)
+                stack.push(v_node);
+            else
+                v_node = stack.popTop();
+        }
+    }
+
+    /**
+     * 广度优先
+     * 邻接表 O(N + E)
+     * 邻接矩阵 O(N^2)
+     */
+    public void BFS(){
+
+    }
+
+    private void visit(Node node){
+        pn(node.data + " ");
+    }
+
 }
--- src/main/java/com/james/ds/graph/Test.java
+++ src/main/java/com/james/ds/graph/Test.java
@@ -6,19 +6,25 @@ import static com.james.ds.Utils.pln;
 public class Test {
 
 	public static void main(String[] args) {
-        Graph g = new Graph(5);
-        g.insert(0, 1);
+        Graph g = new Graph(7);
         g.insert(1, 2);
         g.insert(1, 3);
         g.insert(2, 3);
-        g.insert(3, 4);
+        g.insert(2, 4);
+        g.insert(3, 5);
+        g.insert(5, 6);
         g.print();
 
-        g.insertGraphLink(0, 1);
         g.insertGraphLink(1, 2);
         g.insertGraphLink(1, 3);
-        g.insertGraphLink(2, 3);
-        g.insertGraphLink(3, 4);
+        g.insertGraphLink(1, 5);
+        g.insertGraphLink(2, 5);
+        g.insertGraphLink(3, 6);
+        g.insertGraphLink(5, 4);
+        g.insertGraphLink(6, 4);
         g.printGraphLink();
+        pln();
+
+        g.DFS(1);
 	}
 }
